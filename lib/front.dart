ef4c15985cc463f99f3f2776cc71429ddc7a3294
Pawtter <108756102+Pawtter@users.noreply.github.com>
2022-07-07 16:05:41 -0300

    Funcional
    aeeee
    
    nada otimizado porém

--- lib/front.dart
+++ lib/front.dart
@@ -1,5 +1,6 @@
 //import 'dart:html';
 import 'dart:ui';
+import 'package:contador_/front.dart';
 import 'package:flutter/material.dart';
 import 'logic.dart';
 import 'dart:convert';
@@ -12,61 +13,96 @@ class PaginaPrinc {
   MaterialApp principal() {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Contador Básico'),
-        ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              StatefulBuilder(
-                builder: (BuildContext context, setState) {
-                  return Text(
-                    contador.toString(),
-                    style: const TextStyle(
-                        fontSize: 100, color: Colors.blueAccent),
-                  );
-                },
-              ),
-              StatefulBuilder(
-                builder: (BuildContext context, setState) {
-                  return ElevatedButton(
-                    onPressed: () => {
-                      setState(
-                        () {
-                          contador = operacao.Adicao(contador) as int;
-                        },
-                      )
-                    },
-                    child: const Text('Adicionar'),
-                  );
-                },
-              ),
-              StatefulBuilder(
-                builder: (BuildContext context, setState) {
-                  return ElevatedButton(
-                    onPressed: () => {
-                      setState(
-                        () {
-                          contador--;
-                        },
-                      )
-                    },
-                    child: const Text('Subtrair'),
-                  );
-                },
-              ),
-
-              //ElevatedButton(
-              //  onPressed: () => {},
-              //  child: const Text('Subtrair'),
-              //)
-            ],
+          appBar: AppBar(
+            title: const Text('Contador Básico'),
           ),
-        ),
+          body: Center(
+            child: contadorCentral(),
+          )
+          //            StatefulBuilder(
+          //              builder: (BuildContext context, setState) {
+          //                return contadorCentral();
+          //              },
+          //            ),
+          //            StatefulBuilder(
+          //              builder: (BuildContext context, setState) {
+          //                return ElevatedButton(
+          //                  onPressed: () => {
+          //                    setState(
+          //                      () {
+          //                        contador = operacao.Adicao(contador);
+          //                      },
+          //                    )
+          //                  },
+          //                  child: const Text('Adicionar'),
+          //                );
+          //              },
+          //            ),
+          //            StatefulBuilder(
+          //              builder: (BuildContext context, setState) {
+          //                return ElevatedButton(
+          //                  onPressed: () => {
+          //                    setState(
+          //                      () {
+          //                        contador--;
+          //                      },
+          //                    )
+          //                  },
+          //                  child: const Text('Subtrair'),
+          //                );
+          //              },
+          //            ),
+
+          //ElevatedButton(
+          //  onPressed: () => {},
+          //  child: const Text('Subtrair'),
+          //)
+
+          ),
+    );
+  }
+}
+
+//----
+
+class contadorCentral extends StatefulWidget {
+  contadorCentral({Key? key}) : super(key: key);
+
+  @override
+  State<contadorCentral> createState() => _contadorCentralState();
+}
+
+class _contadorCentralState extends State<contadorCentral> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text(
+            contador.toString(),
+            style: const TextStyle(fontSize: 100, color: Colors.blueAccent),
+          ),
+          ElevatedButton(
+            onPressed: () => {
+              setState(
+                () {
+                  contador = operacao.Adicao(contador);
+                },
+              )
+            },
+            child: const Text('Adicionar'),
+          ),
+          ElevatedButton(
+            onPressed: () => {
+              setState(
+                () {
+                  contador--;
+                },
+              )
+            },
+            child: const Text('Subtrair'),
+          ),
+        ],
       ),
     );
   }
